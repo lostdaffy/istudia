acf7bd4228058f95cfddf443442900a20c13239f
Satyaprakash <satyaasingh001@gmail.com>
2024-07-16 16:40:57 +0530

    designed Navbar and Home section

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h2>istudia</h2>
+      <div>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
